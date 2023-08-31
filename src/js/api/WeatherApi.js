e8a1d281cffaa990182bd1022ff2d8766bb6857e
gustavomotamacedo <mota.macedo05@gmail.com>
2023-08-31 10:29:11 -0300

    Update WeatherApi.js

--- src/js/api/WeatherApi.js
+++ src/js/api/WeatherApi.js
@@ -18,12 +18,36 @@ class WeatherApi {
 
     setPosition(latitude, longitude) {
         console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-        this.latitude = `${latitude}`
-        this.longitude = `${longitude}`
+        this.latitude = parseFloat(latitude)
+        this.longitude = parseFloat(longitude)
+    }
+
+    get latitude() {
+        return this.latitude
+    }
+
+    get longitude() {
+        return this.longitude
+    }
+
+    set latitude(latitude) {
+        this.latitude = latitude
+    }
+
+    set longitude(longitude) {
+        this.longitude = longitude
     }
 
 }
 
 const api = new WeatherApi()
 
-//const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${api.latitude}&lon=${api.longitude}&exclude=${part}&appid=${API key}`
+
+setTimeout(() => {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${api.latitude}&longitude=${api.longitude}&daily=weathercode,precipitation_sum&current_weather=true&timezone=auto&forecast_days=1`
+    console.log(url)
+    fetch(url)
+        .then((response) => response.json)
+        .then((responseJson) => responseJson.toString)
+        .then((results) => console.log(results))
+}, 5000)
